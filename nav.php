4a9b7acce375cc06b8bd7f0334ead80cb2d38585
ReaV0211 <rea.vertes@student.um.si>
2026-05-19 10:40:45 +0200

    Ločenje nav.php

---
+++ nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="domov.html">
+            <img src="Popa_transparent.png" alt="Popa logo" style="height: 40px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="menu">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="domov.html">Domov</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php">O nas</a></li>
+                <li class="nav-item"><a class="nav-link" href="izdelki.html">Izdelki</a></li>
+                <li class="nav-item"><a class="nav-link" href="kontakt.html">Kontakt</a></li>
+                <li class="nav-item"><a class="nav-link" href="narocilo.html">Naročilo</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
