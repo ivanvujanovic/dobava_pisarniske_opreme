73693f22a0cf11ffe8b983b807f22e57c8cba614
ReaV0211 <rea.vertes@student.um.si>
2026-05-19 10:43:45 +0200

    V kodi domov.php dodavanje header, nav, and footer

--- domov.php
+++ domov.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html lang="sl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Popa - Domov</title>
-    <!-- Provjeri putanju do Bootstrapa i CSS-a! -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="domov.html">
-            <img src="Popa_transparent.png" alt="Popa logo" style="height: 40px;">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-center" id="menu">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="domov.html">Domov</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.html">O nas</a></li>
-                <li class="nav-item"><a class="nav-link" href="izdelki.html">Izdelki</a></li>
-                <li class="nav-item"><a class="nav-link" href="kontakt.html">Kontakt</a></li>
-                <li class="nav-item"><a class="nav-link" href="narocilo.html">Naročilo</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
 
 <section class="hero container py-5">
     <div class="row align-items-center">
@@ -60,6 +31,4 @@
     </div>
 </section>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include 'footer.php'; ?>
